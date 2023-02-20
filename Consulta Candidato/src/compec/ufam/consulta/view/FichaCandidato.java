5c2cc39afff8918e49d5e1f696ddf338066a7b5f
Felipe André <fass@icomp.ufam.edu.br>
2023-02-19 21:28:00 -0400

    Classe 'FichaCandidato' organizada e comentada

--- Consulta Candidato/src/compec/ufam/consulta/view/FichaCandidato.java
+++ Consulta Candidato/src/compec/ufam/consulta/view/FichaCandidato.java
@@ -1,57 +1,63 @@
 package compec.ufam.consulta.view;
 
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.io.IOException;
+
+import java.sql.SQLException;
+
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
 
 import com.phill.libs.ResourceManager;
 
 import compec.ufam.consulta.model.Candidato;
-import net.sf.jasperreports.engine.JasperFillManager;
+
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.util.JRLoader;
 import net.sf.jasperreports.view.JasperViewer;
 
+/** Classe responsável pela geração do relatório 'Ficha de Candidato'.
+ *  @author Felipe André - felipeandresouza@hotmail.com
+ *  @version 2.0, 20/FEV/2023 */
 public class FichaCandidato {
 	
-	public static void main(String[] args) throws Exception {
-		Candidato c = new Candidato();
-		c.setNome("FELIPE");
-		c.setSexo("M");
-		c.setDataNascimento("25061993");
-		c.setDataInscricao("2023-01-01 11:54:32.223");
-		show(c);
-	}
-
-	public static void show(final Candidato candidato) throws Exception {
+	/** Exibe o relatório de 'Ficha de Colaborador' com os dados de um <code>colaborador</code>.
+	 *  @param colaborador - colaborador 
+	 *  @throws IOException quando o arquivo de logo do relatório (res/img/logo.jpg) está inacessível
+	 *  @throws JRException quando há alguma falha no carregamento do relatório Jasper
+	 *  @throws SQLException quando o banco de dados está inacessível */
+	public static void show(final Candidato candidato) throws IOException, JRException, SQLException {
 		
-		/** Leitura dos arquivos */
+		// Leitura dos arquivos
 		File     reportPath = ResourceManager.getResourceAsFile("relatorios/FichaCandidato.jasper");
 		BufferedImage  logo = ImageIO.read(ResourceManager.getResourceAsFile("img/logo.jpg"));
 		JasperReport report = (JasperReport) JRLoader.loadObject(reportPath);
 		
-		/** Preparação dos parâmetros */
+		// Preparação dos parâmetros
 		Map<String, Object> parameters = new HashMap<String,Object>(1);
 		parameters.put("PAR_LOGO", logo);
 		
 		// Preparando o datasource
 		List<Candidato> beans = new ArrayList<Candidato>(1); beans.add(candidato);
 		
-		/** Preenchendo o relatório */
+		// Preenchendo o relatório
 		JasperPrint  prints = JasperFillManager.fillReport(report, parameters, new JRBeanCollectionDataSource(beans, false));
 		prints.setProperty("net.sf.jasperreports.export.xls.ignore.graphics", "true");
 		
-		/** Preparando e exibindo o relatório */
-		JasperViewer jrv = new JasperViewer(prints,false);
-		jrv.setTitle("Ficha de Candidato - " + candidato.getNome());
-		jrv.setVisible(true);
+		// Preparando e exibindo o relatório
+		JasperViewer viewer = new JasperViewer(prints,false);
+		viewer.setTitle("Ficha de Candidato - " + candidato.getNome());
+		viewer.setVisible(true);
 		
 	}
 	
